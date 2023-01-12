283b2a539c35d63f5f878bc52aad463124494c06
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-01-12 09:16:24 +0530

    added listModel

--- lib/user list/Logic/bloc/network fetch/network_fetch_state.dart
+++ lib/user list/Logic/bloc/network fetch/network_fetch_state.dart
@@ -1,6 +1,14 @@
 part of 'network_fetch_bloc.dart';
 
+enum Status { initial, success, failure }
+
 @immutable
-abstract class NetworkFetchState {}
+abstract class NetworkFetchState {
+  late final Status;
+}
 
 class NetworkFetchInitial extends NetworkFetchState {}
+
+class NetworkFetchSuccess extends NetworkFetchState {}
+
+class NetworkFetchFailure extends NetworkFetchState {}
