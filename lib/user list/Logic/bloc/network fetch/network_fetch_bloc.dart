7cf59e9201d07b459713cf37033da3e4df924722
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-01-12 10:19:56 +0530

    added equatable

--- lib/user list/Logic/bloc/network fetch/network_fetch_bloc.dart
+++ lib/user list/Logic/bloc/network fetch/network_fetch_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 import '../../../data/model/list_model.dart';
@@ -7,9 +8,7 @@ part 'network_fetch_event.dart';
 part 'network_fetch_state.dart';
 
 class NetworkFetchBloc extends Bloc<NetworkFetchEvent, NetworkFetchState> {
-  NetworkFetchBloc() : super(NetworkFetchInitial()) {
-    on<NetworkFetchEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  NetworkFetchBloc() : super(const NetworkFetchInitial()) {
+    on<NetworkFetchEvent>((event, emit) {});
   }
 }
